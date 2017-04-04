05da44b485c5cccbf5e096f42310429c1d397296
rbcksdls <rbcksdls@naver.com>
2017-04-04 15:28:22 +0900

    fix bugs and create ignore file

--- calc.c
+++ calc.c
@@ -5,7 +5,8 @@ int main(){
 	FILE *fp = NULL;
 	int operand1, operand2;
 	char operator = ' ';
-	int result, line = 0;
+	float result = 0;
+	int line = 0;
 
 	fp = fopen("read.txt","r");
 	if(fp!=NULL){
@@ -13,21 +14,29 @@ int main(){
 	
 		for(int i=0; i<line; i++) {
 			fscanf(fp, "%d %c %d",&operand1, &operator, &operand2);
+			if (operator == ' ') {
+				return 0;
+			}
 			switch(operator) {
 				case '+':
-				result = add(operand1, operator);
+				result = add(operand1, operand2);
 				break;
 				case '-':
-				result = minus(operand1, operator);
+				result = minus(operand1, operand2);
 				break;
 				case '*':
-				result = mul(operand1, operator);
+				result = mul(operand1, operand2);
+				break;
 				case '/':
-				result = div(operand1, operator);
+				result = div(operand1, operand2);
 				break;
 			}		
 			printf("%d %c %d = %d\n",
 				 operand1, operator, operand2, result);
+			operand1 = 0;
+			operator = ' ';
+			operand2 = 0;
+			result = 0;
 		}
 	}
 	return 0;
