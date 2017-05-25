16185376bb328c214e0700ae4657cddeb6e5b9ef
gkrtjr <fantasytotal@naver.com>
2017-05-25 10:47:14 -0700

    myscanf

--- my_fscanf.c
+++ my_fscanf.c
@@ -5,24 +5,31 @@
 void my_fscanf(FILE *fp, const char *format, ...) {
 	va_list list;
 	va_start(list, format);
-	int c,i;
+	int c;
+	int *i;
+	char *ch;
+	
 	while(*format) {
 		if(*format == '%') {
 			format++;
 			switch(*format) {
 			case 'd':
+				i=va_arg(list,int*);
 				while(isspace(c=getc(fp))){}
-				i=0;
-				while(isdigit(fp)){
-					i=i*10+c-'0';
+				*i=0;
+				while(isdigit(c)){
+					*i=(*i)*10+c-'0';
 					c=getc(fp);
 				}
-				i=va_arg(list,int);
-				ungetc(c,fp);
+				break;
 			case 'c':
-				c=getc(fp);
-				c=va_arg(list,int);
+				ch=va_arg(list,char*);
+				*ch=getc(fp);
+				break;
 			}
+		}
+		else{
+			format++;
 		}
 	}
 	va_end(list);
